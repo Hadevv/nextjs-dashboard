37f8b3c150a8b6f203952a38a45fe66b0ef81446
Hadevv <ademeure29@gmail.com>
2024-01-25 12:14:03 +0100

    dépot git learn nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ui/global.css';
+import { inter,} from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
